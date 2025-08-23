c15ffb8307ffd2a44980bbe47a9b8cf69b2d25f2
T H <timthezombie@gmail.com>
2025-08-23 08:24:22 +0200

    Fruit App morning sync

--- connections.py
+++ connections.py
@@ -7,11 +7,11 @@ from tools import print_and_log, deco_print_and_log
 # Load environment variables from .env file
 load_dotenv()
 
-local_path = os.environ.get("LOCAL_PATH")
+upload_path = os.environ.get("LOCAL_PATH") or ""
 bucket_name = os.environ.get("BUCKET_NAME")
 
 @deco_print_and_log('Upload files')
-def upload_file(source_file_name, bucket_name=bucket_name, local_path=local_path):
+def upload_file(source_file_name, bucket_name=bucket_name, local_path=upload_path):
     """Check if file exists and upload to GCP bucket."""
     full_path = local_path + '\\' + source_file_name
     destination_prefix = 'fruit_project/Datasets'
@@ -22,15 +22,16 @@ def upload_file(source_file_name, bucket_name=bucket_name, local_path=local_path
 
     # Construct list of names to check if file exists
     blob_list = [os.path.basename(blob.name) for blob in bucket.list_blobs(prefix='fruit_project/Datasets')]
-
+    
     if source_file_name in blob_list:
-        print(f"File {source_file_name} already exists in destination bucket {bucket_name}.")
+        print_and_log(f"File {source_file_name} already exists in destination bucket {bucket_name}.")
         return
 
     blob = bucket.blob(destination_blob_name)
 
     try:
+        print_and_log(f"Uploading {source_file_name} to: {full_path}...")
         blob.upload_from_filename(full_path)
-        print(f"File {source_file_name} uploaded to destination bucket {bucket_name}.")
+        print_and_log(f"File {source_file_name} uploaded to destination bucket {bucket_name}.")
     except Exception as e:
         print_and_log(f"Error when uploading file/s: {e}")
--- main.py
+++ main.py
@@ -7,14 +7,14 @@ from tools import print_and_log, deco_print_and_log
 
 load_dotenv()
 
-local_path = Path(os.environ.get("LOCAL_PATH"))
+upload_path = Path(os.environ.get("LOCAL_PATH") or "") 
 
 
 @deco_print_and_log('APP')
 def main():
-    
+
     # Run function for all files in upload folder:
-    [upload_file(x.name) for x in local_path.iterdir()]
+    [upload_file(x.name) for x in upload_path.iterdir()]
 
 
 if __name__ == '__main__':
